fe9116fb26b7e69a0ffdf86e9a418f00a51ea52f
Devin Lin <66978846+thisisnotdevin@users.noreply.github.com>
2021-11-02 20:19:41 -0400

    added comments

--- word-counter/script.js
+++ word-counter/script.js
@@ -4,9 +4,13 @@ const wordCount = document.getElementById("wordCount");
  function wordCounter(){
   
     let text = textBox.value;
+
+    //we need to separate the spaces, they dont count as words
     text = text.trim();
     const words = text.split(" ");
     wordCount.innerText = words.length;
+
+    //makes sure that whenever there are no words in the text-box the count is  0
     if (words[0] == ""){
         wordCount.innerText = 0;
     } else {
